cd5698e6b6d7306ccce828234fa46a4d1dae6617
w3irdrobot <rob@w3ird.tech>
2024-08-24 20:21:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# `ds4432`
+
+[![Crate][crate-image]][crate-link]
+[![Docs][docs-image]][docs-link]
+![AGPVv3 licensed][license-image]
+
+Platform-agnostic Rust driver for the Maxim DS4432 Dual-Channel, I^2C, 7-Bit Sink/Source Current Digital To Analog (DAC) converter.
+
+## Resources
+
+- [DS4432 product page][product-page]
+- [DS4432 datasheet][datasheet]
+
+## License
+
+Distributed under the AGPLv3 License. See [LICENSE.txt](./LICENSE.txt) for more information.
+
+## Support
+
+PRs are more than welcome! I don't know how much more needs to be added, but I'm open to ideas.
+
+Feeling generous? Leave me a tip! ⚡️w3irdrobot@vlt.ge.
+
+Think I'm an asshole but still want to tip? Please donate [to OpenSats](https://opensats.org/).
+
+Want to tell me how you feel? Hit me up [on Nostr](https://njump.me/rob@w3ird.tech).
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the AGPLv3 license, shall
+be licensed as above, without any additional terms or conditions.
+
+[crate-image]: https://img.shields.io/crates/v/ds4432.svg
+[crate-link]: https://crates.io/crates/ds4432
+[docs-image]: https://docs.rs/ds4432/badge.svg
+[docs-link]: https://docs.rs/ds4432/
+[license-image]: https://img.shields.io/badge/license-AGPLv3-blue.svg
+[product-page]: https://www.digikey.com/en/products/detail/analog-devices-inc-maxim-integrated/DS4432U-T-R/2062898
+[datasheet]: https://www.analog.com/media/en/technical-documentation/data-sheets/DS4432.pdf
---
+++ src/lib.rs
@@ -0,0 +1,216 @@
+#![no_std]
+//! DS4432 driver.
+//!
+//! The DS4432 contains two I2C programmable current
+//! DACs that are each capable of sinking and sourcing
+//! current up to 200µA. Each DAC output has 127 sink
+//! and 127 source settings that are programmed using the
+//! I2C interface. The current DAC outputs power up in a
+//! high-impedance state.
+//!
+//! - [DS4432 product page](https://www.digikey.com/en/products/detail/analog-devices-inc-maxim-integrated/DS4432U-T-R/2062898)
+//! - [DS4432 datasheet](https://www.analog.com/media/en/technical-documentation/data-sheets/DS4432.pdf)
+
+#[cfg(feature = "defmt")]
+use defmt::{debug, error, trace, Format};
+
+#[cfg(all(feature = "blocking", feature = "async"))]
+compile_error!("feature \"blocking\" and feature \"async\" cannot be enabled at the same time");
+
+#[cfg(feature = "blocking")]
+use embedded_hal::i2c::I2c;
+#[cfg(feature = "async")]
+use embedded_hal_async::i2c::I2c;
+
+/// The DS4432's I2C addresses.
+const SLAVE_ADDRESS: u8 = 0x90;
+
+/// An output controllable by the DS4432. This device has two.
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum Output {
+    One = 0xF8,
+    Two = 0xF9,
+}
+
+impl From<Output> for u8 {
+    fn from(value: Output) -> Self {
+        value as u8
+    }
+}
+
+/// Driver errors.
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error<E> {
+    /// I2C bus error.
+    I2c(E),
+    /// The given level is too high
+    InvalidLevel(u8),
+}
+
+/// The status of an output.
+#[derive(Debug, Clone, Copy)]
+pub enum Status {
+    /// The output should sink at the given level
+    Sink(u8),
+    /// The output should source at the given level
+    Source(u8),
+    /// The output is completely disabled
+    Disable,
+}
+
+impl Status {
+    pub fn code(&self) -> u8 {
+        match self {
+            Self::Sink(c) | Self::Source(c) => *c,
+            Self::Disable => 0,
+        }
+    }
+}
+
+impl From<Status> for u8 {
+    fn from(value: Status) -> Self {
+        match value {
+            Status::Disable | Status::Sink(0) | Status::Source(0) => 0,
+            // ensures MSB is 0
+            Status::Sink(code) => code & 0x7F,
+            // ensures MSB is 1
+            Status::Source(code) => code | 0x80,
+        }
+    }
+}
+
+impl From<u8> for Status {
+    fn from(value: u8) -> Self {
+        let sourcing = value & 0x80 == 0x80;
+        let code = value & 0x7F;
+
+        match (sourcing, code) {
+            (false, 0) => Self::Disable,
+            (true, c) => Self::Source(c),
+            (false, c) => Self::Sink(c),
+        }
+    }
+}
+
+/// A DS4432 Digital To Analog (DAC) converter on the I2C bus `I`.
+#[maybe_async_cfg::maybe(sync(feature = "blocking", keep_self), async(feature = "async"))]
+pub struct DS4432<I> {
+    i2c: I,
+}
+
+#[maybe_async_cfg::maybe(sync(feature = "blocking", keep_self), async(feature = "async"))]
+impl<I, E> DS4432<I>
+where
+    I: I2c<Error = E>,
+{
+    /// Create a new DS4432 using the given I2C implementation
+    pub async fn new(i2c: I) -> Self {
+        #[cfg(feature = "defmt")]
+        trace!("new");
+
+        Self { i2c }
+    }
+
+    /// Set the current sink/source status and code of an output
+    pub async fn set_status(&mut self, output: Output, status: Status) -> Result<(), Error<E>> {
+        #[cfg(feature = "defmt")]
+        trace!("set_status");
+
+        self.write_reg(output, status).await
+    }
+
+    /// Get the current sink/source status and code of an output
+    pub async fn status(&mut self, output: Output) -> Result<Status, Error<E>> {
+        #[cfg(feature = "defmt")]
+        trace!("status");
+
+        self.read_reg(output).await.map(|v| v.into())
+    }
+
+    /// Read a register value.
+    async fn read_reg<R: Into<u8>>(&mut self, reg: R) -> Result<u8, Error<E>> {
+        #[cfg(feature = "defmt")]
+        trace!("read_reg");
+
+        let mut buf = [0x00];
+        let reg = reg.into();
+
+        self.i2c
+            .write_read(SLAVE_ADDRESS, &[reg], &mut buf)
+            .await
+            .map_err(Error::I2c)?;
+
+        #[cfg(feature = "defmt")]
+        debug!("R @0x{:x}={:x}", reg, buf[0]);
+
+        Ok(buf[0])
+    }
+
+    /// Blindly write a single memory address with a fixed value.
+    async fn write_reg<R, V>(&mut self, reg: R, value: V) -> Result<(), Error<E>>
+    where
+        R: Into<u8>,
+        V: Into<u8>,
+    {
+        #[cfg(feature = "defmt")]
+        trace!("write_reg");
+
+        let reg = reg.into();
+        let value = value.into();
+
+        #[cfg(feature = "defmt")]
+        debug!("W @0x{:x}={:x}", reg, value);
+
+        self.i2c
+            .write(SLAVE_ADDRESS, &[reg, value])
+            .await
+            .map_err(Error::I2c)
+    }
+
+    /// Return the underlying I2C device
+    pub fn release(self) -> I {
+        self.i2c
+    }
+
+    /// Destroys this driver and releases the I2C bus `I`.
+    pub fn destroy(self) -> Self {
+        self
+    }
+}
+
+#[cfg(test)]
+mod test {
+    // extern crate alloc;
+    extern crate std;
+
+    use super::*;
+    use embedded_hal_mock::eh1::i2c;
+    use std::vec;
+
+    #[test]
+    fn can_get_output_1_status() {
+        let expectations = [i2c::Transaction::write_read(0x90, vec![0xF8], vec![0xAA])];
+        let mock = i2c::Mock::new(&expectations);
+        let mut ds4432 = DS4432::new(mock);
+
+        let status = ds4432.status(Output::One).unwrap();
+        assert!(matches!(status, Status::Source(42)));
+
+        let mut mock = ds4432.release();
+        mock.done();
+    }
+
+    #[test]
+    fn can_set_output_2_status() {
+        let expectations = [i2c::Transaction::write(0x90, vec![0xF9, 0x2A])];
+        let mock = i2c::Mock::new(&expectations);
+        let mut ds4432 = DS4432::new(mock);
+
+        // just making sure it doesn't error
+        ds4432.set_status(Output::Two, Status::Sink(42)).unwrap();
+
+        let mut mock = ds4432.release();
+        mock.done();
+    }
+}
